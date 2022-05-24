e3b8d5504eb589552b7e751f3a4bd68f31298126
duhee <engml0228@naver.com>
2022-05-24 23:18:00 +0900

    text 가져오기
    selenium모듈로 기사 본문 text내용 가져올 수 있게 함. 다음할 것 -기사내용에서 불필요한 특수기호들 없애주기

--- naver_news.py
+++ naver_news.py
@@ -1,20 +1,85 @@
+#정규표현식 import re 모듈 이용해서 특수문자 없애기
+
+ 
+
 from selenium import webdriver
+
 from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
+
 from selenium.common.exceptions import NoSuchElementException
+
 from selenium.webdriver.chrome.service import Service
+
 from webdriver_manager.chrome import ChromeDriverManager
 
+ 
 
+ 
 
 #chrome브라우저 열기
+
 def set_chrome_driver():
+
     chrome_options = webdriver.ChromeOptions()
+
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
     return driver
+
 driver=set_chrome_driver()
-driver.implicitly_wait(5)
-driver.get('https://news.naver.com')#네이버창 열기
+
+driver.implicitly_wait(10)
+
+webpage = driver.get('https://news.naver.com')#네이버창 열기
+
 driver.maximize_window() #창 최대
 
-driver.find_element(By.XPATH,'')
+ 
+
+#'경제' 기사들어가기
+
+driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
+
+driver.find_element(By.XPATH,'//*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a').click() #첫번재 묶음에서 기사 1번 제목클릭
+
+text = str(driver.find_element(By.XPATH,'//*[@id="dic_area"]').text)
+
+print(text)
+
+ 
+
+       
+
+ 
+
+ 
+
+"""
+
+기사들이 6개로 4묶음으로 구성되어있음 - 고정되어있는 헤드라인 뉴스 제외하고 한페이지 당 24개 뉴스기사가 있음.
+
+//*[@id="section_body"]/ul[1]/li[1] -> ul[1]에서 "1"은 4묶음중 첫번째 묶음
+
+ 
+
+//*[@id="section_body"]/ul[2]/li[1]  ->2번째 묶음
+
+ 
+
+ 
+
+페이지가 변해도 위의 xpath 태그는 위와같이 같았음.
+
+ 
+
+//*[@id="section_body"]/ul[1]/li[1]/dl/dt[2]/a  ->첫번째 묶음에서 기사1번 제목클릭 
+
+ 
+
+//*[@id="section_body"]/ul[1]/li[2]/dl/dt[2]/a   ->첫번재 묶음에서 기사 2번 제목 클릭
+
+ 
+
+"""
