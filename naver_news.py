c7ac9ae2eb731b8c153a0fc72f53d7f217dfa532
duhee <engml0228@naver.com>
2022-06-03 18:35:48 +0900

    page
    페이지 개수 새기,
    이 방법 말고도 링크에https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=101#&date=%2000:00:00&page=3
    page 부분에 큰 숫자를 넣으면 마지막 페이지로 이동함.

--- naver_news.py
+++ naver_news.py
@@ -6,15 +6,11 @@ from selenium import webdriver
 
 from selenium.webdriver.common.by import By
 
-from selenium.webdriver.common.keys import Keys
-
-from selenium.common.exceptions import NoSuchElementException
-
 from selenium.webdriver.chrome.service import Service
 
 from webdriver_manager.chrome import ChromeDriverManager
 
- 
+from time import sleep 
 
 f = open("./news/news_text.txt","w")
 
@@ -38,10 +34,18 @@ webpage = driver.get('https://news.naver.com')#네이버창 열기
 
 driver.maximize_window() #창 최대
 
+
+#경제 페이지 개수세기
+
+driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
+
+while(1):
+    driver.find_element_by_css_selector("#paging > a._paging.next.nclicks\(air\.next\)").click()
+    sleep(2)
+    
  
-
 #'경제' 기사들어가기
-
+"""
 driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
 
 driver.find_element(By.XPATH,'//*[@id="section_body"]/ul[1]/li[2]/dl/dt[2]/a').click() #첫번재 묶음에서 기사 1번 제목클릭
@@ -64,7 +68,6 @@ def clean_text(news_text):
 
   return text
 
- 
 
 news_text = clean_text(text)
 
@@ -78,7 +81,7 @@ f.close()
 
  
 
- 
+ """
 
  
 
