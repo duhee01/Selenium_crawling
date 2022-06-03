4fccf0d3ed01c0441aed1ba08649f05e216c57dc
duhee <engml0228@naver.com>
2022-06-03 18:37:24 +0900

    Update naver_news.py

--- naver_news.py
+++ naver_news.py
@@ -34,7 +34,7 @@ webpage = driver.get('https://news.naver.com')#네이버창 열기
 
 driver.maximize_window() #창 최대
 
-
+"""
 #경제 페이지 개수세기
 
 driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
@@ -42,10 +42,10 @@ driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/di
 while(1):
     driver.find_element_by_css_selector("#paging > a._paging.next.nclicks\(air\.next\)").click()
     sleep(2)
-    
+"""   
  
 #'경제' 기사들어가기
-"""
+
 driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
 
 driver.find_element(By.XPATH,'//*[@id="section_body"]/ul[1]/li[2]/dl/dt[2]/a').click() #첫번재 묶음에서 기사 1번 제목클릭
@@ -54,9 +54,6 @@ text = str(driver.find_element(By.XPATH,'//*[@id="dic_area"]').text)
 
 print(text)
 
- 
-
- 
 
  
 
@@ -81,7 +78,6 @@ f.close()
 
  
 
- """
 
  
 
