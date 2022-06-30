5b35c801c6a50024bc3eed9bcb04fdf012760250
duhee <engml0228@naver.com>
2022-06-30 22:43:33 +0900

    naver_news

--- naver_news.py
+++ naver_news.py
@@ -1,30 +1,35 @@
 #정규표현식 import re 모듈 이용해서 특수문자 없애기
 
 import re
-
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
-
 from selenium.webdriver.chrome.service import Service
-
 from webdriver_manager.chrome import ChromeDriverManager
-
 from time import sleep 
+from datetime import datetime
+import pandas as pd
 
-f = open("./news/news_text.txt","w")
 
- 
+dates = []
+titles = []
+contents = []
 
 #chrome브라우저 열기
 
 def set_chrome_driver():
-
     chrome_options = webdriver.ChromeOptions()
-
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-
     return driver
+
+def clean_text(news_text):
+    text = re.sub('[-=+,#/\?:^.@*\"※~【▶◀ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', news_text)
+    text = text.replace("\n","",1000)
+    return text
+
+def clean_date(date_text):
+    date = datetime.strptime(date_text, '%Y.%m.%d')
+    date = date.strftime('%Y-%m-%d')
+    return date
 
 driver=set_chrome_driver()
 
@@ -49,47 +54,27 @@ while(1):
 driver.find_element(By.XPATH,'/html/body/section/header/div[2]/div/div/div[1]/div/div/ul/li[3]/a/span').click()
 
 driver.find_element(By.XPATH,'//*[@id="section_body"]/ul[1]/li[2]/dl/dt[2]/a').click() #첫번재 묶음에서 기사 1번 제목클릭
-
+#날짜
+date_time = driver.find_element(By.XPATH, '//*[@id="ct"]/div[1]/div[3]/div[1]/div/span').text[:10]
+date = clean_date(date_time)
+dates.append(date)
+print(dates)
+#제목
+title = driver.find_element(By.XPATH, '//*[@id="ct"]/div[1]/div[2]/h2').text[:]
+titles.append(title)
+print(titles)
+#본문
 text = str(driver.find_element(By.XPATH,'//*[@id="dic_area"]').text)
-
-print(text)
-
-
- 
-
-def clean_text(news_text):
-
-  text = re.sub('[-=+,#/\?:^.@*\"※~【▶◀ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', news_text)
-
-  text = text.replace("\n","",1000)
-
-  return text
-
-
 news_text = clean_text(text)
+contents.append(news_text)
+print(contents)
+driver.back()
 
-#print(news_text)
+    
+news_df = pd.DataFrame({'title':titles,'date':dates,'content':contents})
 
-#텍스트파일에 쓰기
+news_df.to_csv('C:\\Users\\user\\OneDrive\\문서\\GitHub\\Beautifulsoup_base\\news\\NaverNews.csv',index=False,encoding='utf-8-sig')
 
-f.write(news_text)
-
-f.close()
-
- 
-
-
- 
-
-"""
-
-txt파일로 저장해서
-
-news목록 만들기
-
-"""
-
- 
 
 """
 
